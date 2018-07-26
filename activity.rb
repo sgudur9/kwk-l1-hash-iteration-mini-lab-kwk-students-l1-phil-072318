1ca2fbc118e28f192b5f53433386f27936922654
sgudur9 <18sgudur.stem@gmail.com>
2018-07-26 14:30:09 +0000

    automatically backed up by learn

--- activity.rb
+++ activity.rb
@@ -2,4 +2,10 @@ puts "Hello! What is your first value? (only accepts decimals)"
 value1 = gets.chomp.to_f
 
 puts "Great! What is your second value? (please use decimals)"
-  if calculate 
+value2 = gets.chomp.to_f
+
+puts "Enter the keyboard symbol of your calculation."
+symbol = gets.chomp
+
+if symbol == "+"
+  puts "Your answer is "
